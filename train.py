2982a5b93d5c82b28721018d0ad8a2ae955ae1a9
yizt <csuyzt@163.com>
2019-12-22 22:34:19 +0800

    update train

--- train.py
+++ train.py
@@ -6,59 +6,21 @@
  @Description    :
 """
 
-import random
+import argparse
 import sys
-import cv2
+
 import numpy as np
 import torch
 from torch import optim
 from torch.nn import CTCLoss
 from torch.utils.data.dataloader import DataLoader
-from torch.utils.data.dataset import Dataset
 from tqdm import tqdm
-# import torchvision.transforms as transforms
 
 import crnn
-import argparse
+from generator import Generator
 
 
-def random_color():
-    return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-
-
-class Generator(Dataset):
-    def __init__(self):
-        super(Generator, self).__init__()
-        self.alpha = ' 0123456789abcdefghijklmnopqrstuvwxyz'
-        self.max_len = 20
-        self.min_len = 10
-
-    def __getitem__(self, item):
-        image = np.random.rand(32, 512, 3) * 255
-        # image = np.zeros((32, 512, 3))
-        image = image.astype(np.uint8)
-        target_len = int(np.random.uniform(self.min_len, self.max_len, size=1))
-        indices = np.random.choice(range(1, len(self.alpha)), target_len)
-        text = [self.alpha[idx] for idx in indices]
-        color = random_color()
-        # print(color)
-        # cv2.imwrite('a.jpg', image)
-        # print(''.join(text))
-        image = cv2.putText(image, ''.join(text), (32, 26), cv2.FONT_HERSHEY_PLAIN, 2.3, color,
-                            thickness=2)
-        # cv2.imwrite('{}.jpg'.format(np.random.randint(10)), image)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        image = np.transpose(image[:, :, np.newaxis], axes=(2, 1, 0))  # [H,W,C]=>[C,W,H]
-        image = image.astype(np.float32) / 255.
-        image -= 0.5
-        image /= 0.5
-        target = np.zeros(shape=(self.max_len,), dtype=np.long)
-        target[:target_len] = indices
-        input_len = 31 - 2
-        return image, target, input_len, target_len
-
-    def __len__(self):
-        return 10000
+# import torchvision.transforms as transforms
 
 
 def train(args):
