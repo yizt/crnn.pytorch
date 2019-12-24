a0467d6345f48766a8ed3bf1ba23e62f18344070
yizuotian <csuyzt@163.com>
2019-12-24 11:42:12 +0800

    更新generator;a)文字和背景像素值区分;b)使用不同尺寸文字

--- generator.py
+++ generator.py
@@ -16,23 +16,22 @@ from data.words import Word
 
 
 def random_color():
-    return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    return [random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)]
 
 
-def put_text(image, x, y, text, color=None):
+def put_text(image, x, y, text, font, color=None):
     """
     写中文字
     :param image:
     :param x:
     :param y:
     :param text:
+    :param font:
     :param color:
     :return:
     """
 
     im = Image.fromarray(image)
-    font = ImageFont.truetype('fonts/STHeiti Light.ttc',
-                              size=30)
     draw = ImageDraw.Draw(im)
     color = (255, 0, 0) if color is None else color
     draw.text((x, y), text, color, font=font)
@@ -44,17 +43,22 @@ class Generator(Dataset):
         super(Generator, self).__init__()
         # self.alpha = ' 0123456789abcdefghijklmnopqrstuvwxyz'
         self.alpha = Word().get_all_words()
-        self.max_len = 16
         self.min_len = 5
+        self.max_len_list = [16, 19, 24, 26]
+        self.max_len = max(self.max_len_list)
+        self.font_size_list = [30, 25, 20, 18]
+        self.font_list = [ImageFont.truetype('fonts/simsun.ttc', size=s) for s in self.font_size_list]
 
     def gen_image(self):
+        idx = np.random.randint(len(self.max_len_list))
         image = np.random.rand(32, 512, 3) * 100
         image = image.astype(np.uint8)
-        target_len = int(np.random.uniform(self.min_len, self.max_len, size=1))
+        target_len = int(np.random.uniform(self.min_len, self.max_len_list[idx], size=1))
         indices = np.random.choice(range(1, len(self.alpha)), target_len)
         text = [self.alpha[idx] for idx in indices]
         color = random_color()
-        image = put_text(image, 32, 1, ''.join(text), tuple(color))
+        image = put_text(image, 32, np.random.randint(1, 32 - self.font_size_list[idx]), ''.join(text),
+                         self.font_list[idx], tuple(color))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         if random.random() > 0.5:
             image = 255 - image
@@ -72,14 +76,14 @@ class Generator(Dataset):
         return image, target, input_len, target_len
 
     def __len__(self):
-        return len(self.alpha)
+        return len(self.alpha) * 100
 
 
 def test_image_gen():
     gen = Generator()
-    im, _, target_len = gen.gen_image()
+    im, indices, target_len = gen.gen_image()
     cv2.imwrite('examples.jpg', im)
-    print(target_len)
+    print(''.join([gen.alpha[i] for i in indices]))
 
 
 if __name__ == '__main__':
