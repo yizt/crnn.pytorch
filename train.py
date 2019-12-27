c8f0593b4e6c13c00eb7eee7afec42c1300cd177
yizuotian <csuyzt@163.com>
2019-12-27 14:38:12 +0800

    update train

--- train.py
+++ train.py
@@ -19,6 +19,7 @@ from tqdm import tqdm
 import crnn
 from generator import Generator
 from config import cfg
+import utils
 
 
 # import torchvision.transforms as transforms
@@ -46,20 +47,44 @@ def init_distributed_mode(args):
         args.rank = os.environ['RANK']
         torch.cuda.set_device(args.local_rank)
 
-        print("args.local_rank:{},RANK:{},WORLD_SIZE:{}".format(args.local_rank, os.environ["RANK"],
-                                                                os.environ['WORLD_SIZE']))
         # args.local_rank, os.environ["RANK"],os.environ['WORLD_SIZE'] 会自动赋值
+        # print("args.local_rank:{},RANK:{},WORLD_SIZE:{}".format(args.local_rank, os.environ["RANK"],
+        #                                                         os.environ['WORLD_SIZE']))
         print('| distributed init (rank {}): {}'.format(
             args.rank, args.dist_url), flush=True)
         torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                              world_size=args.world_size, rank=args.rank)
-        setup_for_distributed(args.rank == 0)
+        setup_for_distributed(args.local_rank == 0)
+
+
+def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, args):
+    epoch_loss = 0.0
+    for image, target, input_len, target_len in tqdm(data_loader):
+        image = image.to(device)
+        # print(target, target_len, input_len)
+        outputs = model(image.to(torch.float32))  # [B,N,C]
+        outputs = torch.log_softmax(outputs, dim=2)
+        outputs = outputs.permute([1, 0, 2])  # [N,B,C]
+        loss = criterion(outputs[2:], target, input_len, target_len)
+        # 梯度更新
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # 当前轮的loss
+        epoch_loss += loss.item() * image.size(0)
+        if np.isnan(loss.item()):
+            print(target, input_len, target_len)
+
+    # 打印日志,保存权重
+    print('Epoch: {}/{} loss: {:03f}'.format(epoch + 1, args.epochs, epoch_loss / len(data_loader.dataset)))
 
 
 def train(args):
+    init_distributed_mode(args)
+    print(args)
     device = torch.device(
         'cuda:{}'.format(args.local_rank) if args.device == 'cuda' and torch.cuda.is_available() else 'cpu')
-    init_distributed_mode(args)
+    torch.backends.cudnn.benchmark = True
     # data loader
     data_set = Generator(cfg.word.get_all_words())
     if args.distributed:
@@ -70,65 +95,73 @@ def train(args):
                              num_workers=args.workers)
     # model
     model = crnn.CRNN(len(data_set.alpha))
+    model = model.to(device)
     criterion = CTCLoss()
     criterion = criterion.to(device)
-    # 加载与训练模型
-    if args.init_epoch > 0:
-        model.load_state_dict(torch.load('crnn.{:03d}.pth'.format(args.init_epoch)))
-    model = model.to(device)
+
     if args.distributed and args.sync_bn:
         model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9,
-                          weight_decay=5e-4)
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
 
     model_without_ddp = model
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank])
         model_without_ddp = model.module
+    # 加载预训练模型
+    if args.init_epoch > 0:
+        checkpoint = torch.load(os.path.join(args.output_dir, 'crnn.{:03d}.pth'.format(args.init_epoch)),
+                                map_location='cpu')
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        model_without_ddp.load_state_dict(checkpoint['model'])
+
     # train
     model.train()
 
     for epoch in range(args.init_epoch, args.epochs):
-        epoch_loss = 0.0
         if args.distributed:
             train_sampler.set_epoch(epoch)
-        for image, target, input_len, target_len in tqdm(data_loader):
-            image = image.to(device)
-            # print(target, target_len, input_len)
-            outputs = model(image.to(torch.float32))  # [B,N,C]
-            outputs = torch.log_softmax(outputs, dim=2)
-            outputs = outputs.permute([1, 0, 2])  # [N,B,C]
-            loss = criterion(outputs[2:], target, input_len, target_len)
-            # 梯度更新
-            model.zero_grad()
-            loss.backward()
-            optimizer.step()
-            # 当前轮的loss
-            epoch_loss += loss.item() * image.size(0)
-            if np.isnan(loss.item()):
-                print(target, input_len, target_len)
-        # 打印日志,保存权重
-        print('Epoch: {}/{} loss: {:03f}'.format(epoch + 1, args.epochs, epoch_loss / len(data_set)))
-        torch.save(model_without_ddp.state_dict(), 'crnn.{:03d}.pth'.format(epoch + 1))
+
+        train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, args)
+        lr_scheduler.step(epoch)
+        # 保存模型
+        if args.output_dir:
+            checkpoint = {
+                'model': model_without_ddp.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': lr_scheduler.state_dict(),
+                'epoch': epoch + 1,
+                'args': args}
+            utils.save_on_master(
+                checkpoint,
+                os.path.join(args.output_dir, 'crnn.{:03d}.pth'.format(epoch + 1)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", type=str, default='cpu', help="cpu or cuda")
     parser.add_argument("--batch-size", type=int, default=64, help="batch size")
-    parser.add_argument("--epochs", type=int, default=40, help="epochs")
+    parser.add_argument("--epochs", type=int, default=90, help="epochs")
     parser.add_argument("--init-epoch", type=int, default=0, help="init epoch")
-    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate")
-    parser.add_argument("--workers", type=int, default=4, help="number of workers")
+    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate")
+    parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('--lr-step-size', default=30, type=int, help='decrease lr every step-size epochs')
+    parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
 
+    parser.add_argument("--workers", type=int, default=4, help="number of workers")
+    parser.add_argument('--output-dir', default='./output', help='path where to save')
+
+    # distributed training parameters
     parser.add_argument(
         "--sync-bn",
         dest="sync_bn",
         help="Use sync batch norm",
         action="store_true",
     )
-
-    # distributed training parameters
     parser.add_argument('--local_rank', type=int, default=0)
     parser.add_argument('--world-size', default=1, type=int,
                         help='number of distributed processes')
