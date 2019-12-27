e6b25ec7c9c7ffb6eafca37c1283f5ae9e9399b4
yizuotian <csuyzt@163.com>
2019-12-27 14:37:46 +0800

    add utils

---
+++ utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : utils.py
+ @Time    : 2019-12-25 11:39
+ @Author  : yizuotian
+ @Description    :
+"""
+import torch
+import torch.distributed as dist
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def save_on_master(*args, **kwargs):
+    if is_main_process():
+        torch.save(*args, **kwargs)
