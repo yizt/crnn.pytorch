c4f069813e48310abaf672d60fbf4a4959dc6a67
yizuotian <csuyzt@163.com>
2020-01-03 16:42:35 +0800

    resize预测图像

--- demo.py
+++ demo.py
@@ -12,7 +12,25 @@ import crnn
 from PIL import Image
 import numpy as np
 import torch
+import cv2
 from config import cfg
+
+
+def load_image(image_path):
+    image = np.array(Image.open(image_path))
+    h, w = image.shape[:2]
+    if h != 32:
+        new_w = int(w * 32 / h)
+        image = cv2.resize(image, (new_w, 32))
+
+    image = Image.fromarray(image).convert('L')
+    # cv2.imwrite(image_path, np.array(image))
+    image = np.array(image).T  # [W,H]
+    image = image.astype(np.float32) / 255.
+    image -= 0.5
+    image /= 0.5
+    image = image[np.newaxis, np.newaxis, :, :]  # [B,C,W,H]
+    return image
 
 
 def main(args):
@@ -20,13 +38,8 @@ def main(args):
     net = crnn.CRNN(num_classes=len(alpha))
     net.load_state_dict(torch.load(args.weight_path, map_location='cpu')['model'])
     net.eval()
-
-    image = Image.open(args.image_path).convert('L')
-    image = np.array(image).T  # [W,H]
-    image = image.astype(np.float32) / 255.
-    image -= 0.5
-    image /= 0.5
-    image = image[np.newaxis, np.newaxis, :, :]
+    # load image
+    image = load_image(args.image_path)
     image = torch.FloatTensor(image)
 
     predict = net(image)[0].detach().numpy()  # [W,num_classes]
