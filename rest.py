89a4fd77a795e05a3eb062537a4364fd1bf20fc9
yizt <csuyzt@163.com>
2020-04-06 11:00:08 +0800

    增加restful服务

---
+++ rest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : rest.py
+ @Time    : 2020/4/6 上午9:39
+ @Author  : yizuotian
+ @Description    :
+""" 
---
+++ rest_test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : rest_test.py
+ @Time    : 2020/4/6 上午10:12
+ @Author  : yizuotian
+ @Description    :
+""" 
