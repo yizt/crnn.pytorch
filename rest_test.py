4c85fcb5afe93216466698be84213d871f5fdb85
yizt <csuyzt@163.com>
2020-04-06 11:36:56 +0800

    restful服务测试

--- rest_test.py
+++ rest_test.py
@@ -4,4 +4,18 @@
  @Time    : 2020/4/6 上午10:12
  @Author  : yizuotian
  @Description    :
-""" 
+"""
+
+import codecs
+
+import cv2
+import requests
+
+img = cv2.imread('./images/horizontal-002.jpg', 0)
+h, w = img.shape
+data = {'img': codecs.encode(img.tostring(), 'base64'),  # 转为字节,并编码
+        'shape': [h, w]}
+
+r = requests.post("http://localhost:5000/crnn", data=data)
+
+print(r.json()['text'])
